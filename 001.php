2127541bf53ef728a02a5abecf2211b8bad96327
Fadlian17 <alfansyahfadlian@gmail.com>
2020-07-13 15:33:53 +0700

    fixed task5 week5

--- 001.php
+++ 001.php
@@ -1,30 +1,48 @@
 <?php
 
-$headlines = "https://www.kompas.com/";
+require_once './simplehtmdom/simple_html_dom.php';
 
-$request = function ($headlines) {
-    // init 
-    $ch = curl_init();
-    // setUrl
-    curl_setopt($ch, CURLOPT_URL, $headlines);
-    // return string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    // curl_close($ch);
-    return $output;
-};
+class News
+{
 
-$document = new DOMDocument();
-libxml_use_internal_errors(true); // disable libxml error
-$document->loadHTML($request($headlines));
-$finder = new DOMXPath($document);
-$article = $finder->query("//h3[contains(@class, 'article__title article__title--medium')]");
+    protected function request($headlines)
+    {
+        // init 
+        $ch = curl_init();
+        // setUrl
+        curl_setopt($ch, CURLOPT_URL, $headlines);
+        // return string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        // curl_close($ch);
+        return $output;
+    }
 
-foreach ($article as  $articles) {
-    echo "[title]: $articles->nodeValue\n", JSON_PRETTY_PRINT;
-    foreach ($articles->firstChild->attributes as $attr) {
-        if ($attr->name == 'href') {
-            echo "[url]: $attr->value\n\n";
+    protected function domDocument($headlines)
+    {
+        $document = new DOMDocument();
+        libxml_use_internal_errors(true); // disable libxml error
+        $document->loadHTML($this->request($headlines));
+        $finder = new DOMXPath($document);
+        return $finder;
+    }
+
+    public function headlineNews($headlines)
+    {
+        $req = $this->domDocument($headlines);
+        $article = $req->query("//h3[contains(@class, 'article__title article__title--medium')]");
+
+
+        foreach ($article as  $articles) {
+            echo "[title]: $articles->nodeValue\n";
+            foreach ($articles->firstChild->attributes as $attr) {
+                if ($attr->name == 'href') {
+                    echo "[url]: $attr->value\n\n";
+                }
+            }
         }
     }
 }
+$article = new News();
+echo "==========Berita Kompas.com=========";
+$article->headlineNews($headlines = 'https://www.kompas.com/');
--- 004.php
+++ 004.php
@@ -1,101 +1,117 @@
 <?php
 
+
 require_once './simplehtmdom/simple_html_dom.php';
-require_once './dompdf/autoload.inc.php';
+require './dompdf/autoload.inc.php';
 
 use Dompdf\Dompdf;
 
-$movie_theater = 'https://www.cgv.id/en/loader/home_movie_list';
+class CGVMovies
+{
+
+    protected function getCinemas($link)
+    {
+        print("Wait for Get url movie...\n");
+        $html = file_get_html($link);
+        $getLink = $html->find('a');
+
+        $link = [];
+        foreach ($getLink as $get) {
+            $links = $get->href;
+            $repQuot = str_replace('"', '', $links);
+            $repSlash = str_replace('\\', '', $repQuot);
+            array_push($link, "https://www.cgv.id" . $repSlash);
+        }
+        print("url movie success! \n");
+        return $link;
+    }
+
+    protected function getMoviesScript($links)
+    {
+        print("Get Movie Data...\n");
+        $all = [];
+        foreach ($links as $link) {
+
+            $result = [];
+            $data_cinemas = file_get_html($link);
+
+            /* GET HTML CODE */
+            $result['title'] = $data_cinemas->find('div.movie-info-title', 0)->innertext;
+            $result['info'] = $data_cinemas->find('div.movie-add-info', 0)->innertext;
+            $result['synopsis'] = $data_cinemas->find('div.movie-synopsis', 0)->innertext;
+            $result['image'] = $data_cinemas->find('div.poster-section', 0)->innertext;
+
+            $all[] = $result;
+        }
+        print("Movie Data success!\n");
+        return $all;
+    }
 
 
-$movies = function ($movie_theater) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $movie_theater);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    // $decode = json_decode($output, true);
-    curl_close($ch);
-    return $output;
-};
+    //memuat dompdf
+    public function htmlpdf($linkAll)
+    {
+        $content = '<h1><center style="font-family: sans-serif">GET INFORMATION MOVIES</center></h1><br><br>';
 
-print("Getting url movie...\n");
-$html = file_get_html($movie_theater);
-$getLink = $html->find('a');
+        $this->GenerateImages();
 
-/* Looping for replace junk symbol  */
-$link_movie = [];
-foreach ($getLink as $get) {
-    $links = $get->href;
+        foreach ($linkAll as $movie) {
+            $xpath = new DOMXPath(@DOMDocument::loadHTML($movie['image']));
+            $src = $xpath->evaluate("string(//img/@src)");
+            $filename = basename($src);
 
-    $repQuot = str_replace('"', '', $links);
-    $repSlash = str_replace('\\', '', $repQuot);
+            $dir = 'image/';
+            $saveloc = $dir . $filename;
+            file_put_contents($saveloc, file_get_contents($src));
 
-    array_push($link_movie, "https://www.cgv.id" . $repSlash);
+            $content .= '<center><img src="image/' . $filename . '"></center>';
+            $content .= "<p><center style='font-family: sans-serif'><h3>{$movie['title']}</h3></center></p>";
+            $content .= $movie['info'];
+            $content .= "<h4>Synopsis</h4> <p style='margin:none'>{$movie['synopsis']}</p>";
+        }
+
+        return $content;
+    }
+
+    //hasil pdf dari cgv
+    public function pdfResults($link)
+    {
+        $getCinemas = $this->getCinemas($link);
+        $getData = $this->getMoviesScript($getCinemas);
+        $htmlPdf = $this->htmlpdf($getData);
+
+        print("Please Wait To Saving Into PDF File...\n");
+
+        /** save to pdf */
+        $dompdf = new Dompdf();
+        $dompdf->load_html($htmlPdf);
+        $dompdf->setPaper('A4', 'potrait');
+        $dompdf->render();
+        $r = $dompdf->output();
+
+        $pdf = './pdf/movie.pdf';
+        if (file_put_contents($pdf, $r)) {
+            echo "success saved as to folder{$pdf}\n";
+        }
+    }
+
+    public function GenerateImages()
+    {
+        print("Checking Folder Image...\n");
+        sleep(1);
+        if (!file_exists('image/')) {
+            print("Folder Image Not Found!\n");
+            sleep(1);
+            print("Generating Folder Image...\n");
+            mkdir('image');
+            print("Folder Image Success Generated!\n");
+        } else {
+            sleep(1);
+            print("Folder Image Found!\n");
+        }
+    }
 }
 
-print("url movie success! \n");
-// var_dump($link);
-
-
-print("Get Movie Data...\n");
-$all = [];
-for ($i = 0; $i < count($link_movie); $i++) {
-
-    $result = [];
-    $data = file_get_html($link_movie[$i]);
-    $result['title'] = $data->find('div.movie-info-title', 0)->innertext;
-    $result['info'] = $data->find('div.movie-add-info', 0)->innertext;
-    $result['synopsis'] = $data->find('div.movie-synopsis', 0)->innertext;
-    $result['image'] = $data->find('div.poster-section', 0)->innertext;
-
-    $all[] = $result;
-}
-
-print("Movie Data success!\n");
-// $encode = json_encode($all, JSON_PRETTY_PRINT);
-// file_put_contents('test.json', $encode);
-// var_dump($all);
-
-
-/** save to pdf */
-$content = '<h1><center style="font-family: sans-serif">GET INFORMATION MOVIES</center></h1><br><br>';
-
-print("Checking Folder Image...\n");
-if (!file_exists('image/')) {
-    print("Folder Image Not Found!\n");
-    sleep(1);
-    print("Generating Folder Image...\n");
-    mkdir('image');
-    print("Folder Image Success Generated!\n");
-} else {
-    sleep(1);
-    print("Folder Image Found!\n");
-}
-
-foreach ($all as $key => $movie) {
-    $xpath = new DOMXPath(@DOMDocument::loadHTML($movie['image']));
-    $src = $xpath->evaluate("string(//img/@src)");
-    $filename = basename($src);
-
-    $dir = 'image/';
-    $saveloc = $dir . $filename;
-    file_put_contents($saveloc, file_get_contents($src));
-
-    $content .= '<center><img src="image/' . $filename . '"></center>';
-    $content .= "<p><center style='font-family: sans-serif'><h3>{$movie['title']}</h3></center></p>";
-    $content .= $movie['info'];
-    $content .= "<h4>Synopsis</h4> <p style='margin:none'>{$movie['synopsis']}</p>";
-}
-
-print("Please Wait To Saving Into PDF File...\n");
-
-$dompdf = new Dompdf();
-$dompdf->load_html($content);
-$dompdf->setPaper('A4', 'potrait');
-$dompdf->render();
-$r = $dompdf->output();
-
-$pdf = './pdf/movie.pdf';
-if (file_put_contents($pdf, $r)) {
-    echo "success saved location as {$pdf}\n";
-}
+$link = 'https://www.cgv.id/en/loader/home_movie_list';
+$scrapeCinemas = new CGVMovies();
+$scrapeCinemas->pdfResults($link);
